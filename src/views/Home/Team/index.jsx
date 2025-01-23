cfed97eeee4492732c183ea87efc0eca36cb816a
shreyash19-pixel <shreyashp734@gmail.com>
2025-01-23 23:09:24 +0530

    updated

--- src/views/Home/Team/index.jsx
+++ src/views/Home/Team/index.jsx
@@ -6,33 +6,33 @@ import DoctorImg3 from '../../../assets/Home/Team/doctor-3.jpg'
 
 const Team = () => {
   return (
-    <section className='team'>
-        <div className="team__headingWrap">
-            <h2 className='team__headingWrap-heading'>Team of Professionals</h2>
-            <p className='team__headingWrap-subheading'>We are proud to be at the forefront of the prosthetics and orthotics industry in India, with a growing network of clinics and service centers dedicated to providing exceptional care and support.</p>
+    <section className='professional'>
+        <div className="professional__headingWrap">
+            <h2 className='professional__headingWrap-heading'>Team of Professionals</h2>
+            <p className='professional__headingWrap-subheading'>We are proud to be at the forefront of the prosthetics and orthotics industry in India, with a growing network of clinics and service centers dedicated to providing exceptional care and support.</p>
         </div>
-        <div className="team__peopleWrap">
-            <div className="team__peopleWrap-infoCard">
-                <img src = {DoctorImg1} alt = "doctor" className='team__peopleWrap-infoCard-img' />
-                <div className="team__peopleWrap-infoCard-detailsWrap">
-                    <h3 className='team__peopleWrap-infoCard-detailsWrap-name'>Anumpma Sharma</h3>
-                    <p className='team__peopleWrap-infoCard-detailsWrap-designation'>Genral Manager</p>
+        <div className="professional__peopleWrap">
+            <div className="professional__peopleWrap-infoCard">
+                <img src = {DoctorImg1} alt = "doctor" className='professional__peopleWrap-infoCard-img' />
+                <div className="professional__peopleWrap-infoCard-detailsWrap">
+                    <h3 className='professional__peopleWrap-infoCard-detailsWrap-name'>Anumpma Sharma</h3>
+                    <p className='professional__peopleWrap-infoCard-detailsWrap-designation'>Genral Manager</p>
                 </div>
             </div>
 
-            <div className="team__peopleWrap-infoCard">
-                <img src = {DoctorImg2} alt = "doctor" className='team__peopleWrap-infoCard-img' />
-                <div className="team__peopleWrap-infoCard-detailsWrap">
-                    <h3 className='team__peopleWrap-infoCard-detailsWrap-name'>Viplav Tyagi</h3>
-                    <p className='team__peopleWrap-infoCard-detailsWrap-designation'>P&O Professionals</p>
+            <div className="professional__peopleWrap-infoCard">
+                <img src = {DoctorImg2} alt = "doctor" className='professional__peopleWrap-infoCard-img' />
+                <div className="professional__peopleWrap-infoCard-detailsWrap">
+                    <h3 className='professional__peopleWrap-infoCard-detailsWrap-name'>Viplav Tyagi</h3>
+                    <p className='professional__peopleWrap-infoCard-detailsWrap-designation'>P&O Professionals</p>
                 </div>
             </div>
 
-            <div className="team__peopleWrap-infoCard">
-                <img src = {DoctorImg3} alt = "doctor" className='team__peopleWrap-infoCard-img' />
-                <div className="team__peopleWrap-infoCard-detailsWrap">
-                    <h3 className='team__peopleWrap-infoCard-detailsWrap-name'>Radha Khanna</h3>
-                    <p className='team__peopleWrap-infoCard-detailsWrap-designation'>P&O Professionals</p>
+            <div className="professional__peopleWrap-infoCard">
+                <img src = {DoctorImg3} alt = "doctor" className='professional__peopleWrap-infoCard-img' />
+                <div className="professional__peopleWrap-infoCard-detailsWrap">
+                    <h3 className='professional__peopleWrap-infoCard-detailsWrap-name'>Radha Khanna</h3>
+                    <p className='professional__peopleWrap-infoCard-detailsWrap-designation'>P&O Professionals</p>
                 </div>
             </div>
         </div>
